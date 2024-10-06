e283ca1adcbcee76d6844721a3f1c6893c8b57f0
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-10-06 01:21:07 -0400

    [added]: api v1.0

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
-
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 //API that will take the credit card number from the user and it will check whether the credit card number is valid or not
 //If the provided payload is in-valid then the API will return Invalid error message in a JSON Format
+
+type CardNumber struct{
+	CreditNum string `json:"creditnum`
+}
+
+func checkCreditNumber(w http.ResponseWriter, r *http.Request) {
+	var creditNum CardNumber
+	err := json.NewDecoder(r.Body).Decode(&creditNum)
+
+	if err!= nil{
+		http.Error(w,err.Error(),http.StatusBadRequest)
+		return 
+	}
+
+	var cNumber []int
+
+	for _,cNum := range(creditNum.CreditNum){
+		val := int(cNum) - 48
+		cNumber = append(cNumber, val)
+	}
+	fmt.Println(cNumber)
+	isValid := Luhn(cNumber)
+
+	//returns whether the given number is valid or not
+	json.NewEncoder(w).Encode(isValid)
+}
+
 func main() {
-	creditCardNumber := []int{7, 9, 9, 2, 7, 3, 9, 8, 7, 1, 3}
-	fmt.Println(Luhn(creditCardNumber))
+	http.HandleFunc("/", checkCreditNumber)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
